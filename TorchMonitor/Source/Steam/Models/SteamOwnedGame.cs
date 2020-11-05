3957e8bf57c21cabfa61190525a133c3447e8530
ryoka0418@gmail.com <ryoka0418@gmail.com>
2020-11-05 20:47:05 +0900

    code up steam api

---
+++ TorchMonitor/Source/Steam/Models/SteamOwnedGame.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace TorchMonitor.Steam.Models
+{
+    public sealed class SteamOwnedGame
+    {
+        [JsonProperty("appid")]
+        public int AppId { get; private set; }
+
+        [JsonProperty("playtime_forever")]
+        public ulong TotalPlaytimeMinutes { get; private set; }
+
+        [JsonProperty("playtime_2weeks")]
+        public ulong RecentPlaytimeMinutes { get; private set; }
+    }
+}
---
+++ TorchMonitor/Source/Steam/Models/SteamResponseException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace TorchMonitor.Steam.Models
+{
+    public class SteamResponseException : Exception
+    {
+        public HttpStatusCode Code { get; }
+
+        public SteamResponseException(HttpStatusCode code, string message) : base(message)
+        {
+            Code = code;
+        }
+    }
+}
