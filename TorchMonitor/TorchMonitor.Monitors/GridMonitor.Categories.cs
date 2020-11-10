5c82098c4dd61ca22052ba461816378125ffb676
ryoka0418@gmail.com <ryoka0418@gmail.com>
2020-11-11 04:58:13 +0900

    better block counter

---
+++ TorchMonitor/TorchMonitor.Monitors/GridMonitor.Categories.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Sandbox.ModAPI;
+using TorchUtils;
+
+namespace TorchMonitor.Monitors
+{
+    public sealed partial class GridMonitor
+    {
+        static readonly Type[] _functionalBlockCategory =
+        {
+            typeof(IMyFunctionalBlock),
+        };
+
+        static readonly Type[] _conveyorCategory =
+        {
+            typeof(IMyConveyor),
+            typeof(IMyConveyorTube),
+        };
+
+        static readonly Type[] _sorterCategory =
+        {
+            typeof(IMyConveyorSorter),
+        };
+
+        static readonly Type[] _subpartCategory =
+        {
+            typeof(IMyMechanicalConnectionBlock),
+            typeof(IMyShipConnector),
+        };
+
+        static readonly Type[] _pbCategory =
+        {
+            typeof(IMyProgrammableBlock),
+        };
+
+        static readonly Type[] _productionBlockCategory =
+        {
+            typeof(IMyProductionBlock),
+        };
+
+        static readonly Type[] _shipToolCategory =
+        {
+            typeof(IMyShipToolBase),
+        };
+
+        static void CountCategories(object block, IDictionary<string, int> counts)
+        {
+            IncrementIfOfAny(block, counts, _functionalBlockCategory, "functional");
+            IncrementIfOfAny(block, counts, _conveyorCategory, "conveyor");
+            IncrementIfOfAny(block, counts, _sorterCategory, "conveyor_sorter");
+            IncrementIfOfAny(block, counts, _subpartCategory, "subpart");
+            IncrementIfOfAny(block, counts, _pbCategory, "pb");
+            IncrementIfOfAny(block, counts, _productionBlockCategory, "production");
+            IncrementIfOfAny(block, counts, _shipToolCategory, "ship_tool");
+        }
+
+        static void IncrementIfOfAny(object block, IDictionary<string, int> counts, IEnumerable<Type> category, string name)
+        {
+            foreach (var type in category)
+            {
+                if (type.IsInstanceOfType(block))
+                {
+                    counts.Increment(name);
+                    return;
+                }
+            }
+        }
+    }
+}
--- TorchMonitor/TorchMonitor.Monitors/GridMonitor.cs
+++ TorchMonitor/TorchMonitor.Monitors/GridMonitor.cs
@@ -1,24 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using InfluxDb;
 using Intervals;
 using NLog;
 using Sandbox.Game.Entities;
-using Sandbox.Game.Entities.Blocks;
-using Sandbox.Game.GameSystems.Conveyors;
 using Sandbox.Game.World;
 using Sandbox.ModAPI;
 using TorchUtils;
-using VRage.Game.Entity;
 
 namespace TorchMonitor.Monitors
 {
-    public class GridMonitor : IIntervalListener
+    public sealed partial class GridMonitor : IIntervalListener
     {
         static readonly Logger Log = LogManager.GetCurrentClassLogger();
-        static readonly Regex NamePattern = new Regex(@"^(Static|Large|Small)\s(Grid|Ship)\s\d+$");
 
         int? _lastGroupCount;
         int? _lastBlockCount;
@@ -26,142 +22,130 @@ namespace TorchMonitor.Monitors
         public void OnInterval(int intervalsSinceStart)
         {
             if (intervalsSinceStart < 120) return;
+            if (intervalsSinceStart % 60 != 0) return;
 
-            if (intervalsSinceStart % 60 == 0)
+            var allGroups = MyCubeGridGroups.Static.Logical.Groups
+                .Select(g => g.Nodes.Select(n => n.NodeData).ToArray())
+                .ToArray();
+
+            var activeGroups = allGroups
+                .Where(gr => gr.Any(g => !g.IsConcealed()))
+                .ToArray();
+
+            // total
             {
-                var groups = MyCubeGridGroups.Static.Logical.Groups
-                    .Select(g => g.Nodes.Select(n => n.NodeData).ToArray())
-                    .ToArray();
+                var groupCount = allGroups.Length;
+                var groupCountDelta = (groupCount - _lastGroupCount) ?? 0;
+                _lastGroupCount = groupCount;
 
-                var activeGroups = groups.Where(gr => gr.Any(g => !g.IsConcealed())).ToArray();
+                var blockCount = allGroups.SelectMany(g => g).Sum(g => g.BlocksCount);
+                var blockCountDelta = (blockCount - _lastBlockCount) ?? 0;
+                _lastBlockCount = blockCount;
 
-                // total
+                var totalPcu = allGroups.SelectMany(g => g).Sum(g => g.BlocksPCU);
+                var activePcu = activeGroups.SelectMany(g => g).Sum(g => g.BlocksPCU);
+
+                InfluxDbPointFactory
+                    .Measurement("construction_all")
+                    .Field("total_grid_count", groupCount)
+                    .Field("total_block_count", blockCount)
+                    .Field("total_pcu", totalPcu)
+                    .Field("active_grid_count", activeGroups.Length)
+                    .Field("active_pcu", activePcu)
+                    .Write();
+
+                InfluxDbPointFactory
+                    .Measurement("construction_delta")
+                    .Field("grid_count_delta", groupCountDelta)
+                    .Field("block_count_delta", blockCountDelta)
+                    .Write();
+            }
+
+            // get the number of online members per each faction
+            var factionMemberCounts = new Dictionary<string, int>();
+            foreach (var onlinePlayer in MySession.Static.Players.GetOnlinePlayers().ToArray())
+            {
+                if (onlinePlayer == null) continue;
+
+                var faction = MySession.Static.Factions.TryGetPlayerFaction(onlinePlayer.PlayerId());
+                var factionTag = faction?.Tag ?? "<single>";
+
+                factionMemberCounts.Increment(factionTag);
+            }
+
+            var allBlockCategoryCounts = new Dictionary<string, int>();
+
+            // active grids
+            Parallel.ForEach(activeGroups, activeGroup =>
+            {
+                var biggestGrid = activeGroup.GetBiggestGrid();
+                if (biggestGrid?.Closed ?? true) return;
+
+                var groupName = biggestGrid.DisplayName;
+
+                var factionTag = biggestGrid.BigOwners
+                    .Select(o => MySession.Static.Factions.TryGetPlayerFaction(o))
+                    .FirstOrDefault()
+                    ?.Tag ?? "<single>";
+
+                var factionMemberCount = factionMemberCounts.TryGetValue(factionTag, out var c) ? c : 0;
+
+                InfluxDbPointFactory
+                    .Measurement("active_grids")
+                    .Tag("grid_name", groupName)
+                    .Tag("faction_tag", factionTag)
+                    .Field("faction_member_count", factionMemberCount)
+                    .Field("pcu", activeGroup.Sum(g => g.BlocksPCU))
+                    .Field("block_count", activeGroup.Sum(g => g.BlocksCount))
+                    .Write();
+
+                var blockCategoryCounts = new Dictionary<string, int>();
+                foreach (var grid in activeGroup)
+                foreach (var slimBlock in grid.CubeBlocks)
                 {
-                    var groupCount = groups.Length;
-                    var groupCountDelta = (groupCount - _lastGroupCount) ?? 0;
-                    _lastGroupCount = groupCount;
+                    var block = slimBlock?.FatBlock;
+                    if (block?.Closed ?? true) continue;
 
-                    var blockCount = groups.SelectMany(g => g).Sum(g => g.BlocksCount);
-                    var blockCountDelta = (blockCount - _lastBlockCount) ?? 0;
-                    _lastBlockCount = blockCount;
-
-                    var deletableGridCount = groups.Count(g => IsGridGroupDeletable(g));
-
-                    InfluxDbPointFactory
-                        .Measurement("grids")
-                        .Field("total", groupCount)
-                        .Field("total_delta", groupCountDelta)
-                        .Field("deletable", deletableGridCount)
-                        .Field("total_blocks", blockCount)
-                        .Field("total_blocks_delta", blockCountDelta)
-                        .Field("active_total", activeGroups.Length)
-                        .Write();
-                }
-
-                // each active grid
-                Parallel.ForEach(activeGroups, activeGroup =>
-                {
-                    var biggestGrid = activeGroup.GetBiggestGrid();
-                    var groupName = biggestGrid.DisplayName;
-                    var multiBlockGrids = activeGroup.Where(g => !IsWheelGrid(g)).ToArray();
-
-                    var conveyorCount = 0;
-                    var sorterCount = 0;
-                    var endpointCount = 0;
-                    var productionBlockCount = 0;
-                    var programmableBlockCount = 0;
-                    var shipToolCount = 0;
-                    foreach (var multiBlockGrid in multiBlockGrids)
-                    foreach (var slimBlock in multiBlockGrid.CubeBlocks)
+                    // skip disabled (turned off) blocks
+                    if (block is IMyFunctionalBlock functionalBlock)
                     {
-                        var block = slimBlock.FatBlock;
-
-                        // skip inactive
-                        if (block is IMyFunctionalBlock functionalBlock)
-                        {
-                            if (!functionalBlock.IsWorking) continue;
-                            if (!functionalBlock.Enabled) continue;
-                        }
-
-                        if (block is IMyConveyor ||
-                            block is IMyConveyorTube)
-                        {
-                            conveyorCount += 1;
-                        }
-
-                        if (block is MyConveyorSorter)
-                        {
-                            sorterCount += 1;
-                        }
-
-                        if (block is IMyConveyorEndpointBlock endpointBlock)
-                        {
-                            endpointCount += endpointBlock.ConveyorEndpoint.GetLineCount();
-                        }
-
-                        if (block is IMyProductionBlock)
-                        {
-                            productionBlockCount += 1;
-                        }
-
-                        if (block is IMyProgrammableBlock)
-                        {
-                            programmableBlockCount += 1;
-                        }
-
-                        if (block is IMyShipToolBase)
-                        {
-                            shipToolCount += 1;
-                        }
+                        if (!functionalBlock.IsWorking) continue;
+                        if (!functionalBlock.Enabled) continue;
                     }
 
-                    var factionTag = biggestGrid.BigOwners
-                        .Select(o => MySession.Static.Factions.TryGetPlayerFaction(o))
-                        .FirstOrDefault()
-                        ?.Tag ?? "<single>";
+                    CountCategories(block, blockCategoryCounts);
+                    CountCategories(block, allBlockCategoryCounts);
+                }
 
-                    InfluxDbPointFactory
-                        .Measurement("active_grids")
+                if (blockCategoryCounts.Any())
+                {
+                    var blockCategoryCountsPoint = InfluxDbPointFactory
+                        .Measurement("block_category_count_per_active_grid")
                         .Tag("grid_name", groupName)
                         .Tag("faction_tag", factionTag)
-                        .Field("pcu", multiBlockGrids.Sum(g => g.BlocksPCU))
-                        .Field("block_count", multiBlockGrids.Sum(g => g.BlocksCount))
-                        .Field("subgrid_count", multiBlockGrids.Length - 1)
-                        .Field("conveyor_count", conveyorCount)
-                        .Field("sorter_count", sorterCount)
-                        .Field("endpoint_count", endpointCount)
-                        .Field("production_block_count", productionBlockCount)
-                        .Field("programmable_block_count", programmableBlockCount)
-                        .Field("ship_tool_count", shipToolCount)
-                        .Write();
-                });
+                        .Field("faction_member_count", factionMemberCount);
+
+                    foreach (var (categoryName, count) in blockCategoryCounts)
+                    {
+                        blockCategoryCountsPoint.Field(categoryName, count);
+                    }
+
+                    blockCategoryCountsPoint.Write();
+                }
+            });
+
+            if (allBlockCategoryCounts.Any())
+            {
+                var allBlockCategoryCountPoint = InfluxDbPointFactory
+                    .Measurement("block_category_count_all_active_grids");
+
+                foreach (var (categoryName, count) in allBlockCategoryCounts)
+                {
+                    allBlockCategoryCountPoint.Field(categoryName, count);
+                }
+
+                allBlockCategoryCountPoint.Write();
             }
-        }
-
-        static bool IsGridGroupDeletable(IEnumerable<MyCubeGrid> group)
-        {
-            // Unowned grids should be deleted
-            var owners = group.SelectMany(g => g.BigOwners.Concat(g.SmallOwners));
-            if (!owners.Any()) return true;
-
-            // Unnamed grids should be deleted
-            if (group.All(g => IsUnnamed(g))) return true;
-
-            return false;
-        }
-
-        static bool IsUnnamed(MyEntity grid)
-        {
-            return NamePattern.IsMatch(grid.DisplayName);
-        }
-
-        static bool IsWheelGrid(MyCubeGrid grid)
-        {
-            var blocks = grid.CubeBlocks;
-            if (blocks.Count != 1) return false;
-
-            var block = blocks.ElementAt(0);
-            return block.FatBlock is MyWheel;
         }
     }
 }
