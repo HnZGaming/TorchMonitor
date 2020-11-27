8b92006a89e8fc4053e74726112e70b9152ed91f
ryo <ryoka0418@gmail.com>
2020-11-27 19:12:55 +0900

    get player ip

---
+++ TorchMonitor/TorchMonitor.Reflections/MySteamGameService_Networking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using VRage.GameServices;
+using VRage.Steam;
+
+namespace TorchMonitor.Reflections
+{
+    public static class MySteamGameService_Networking
+    {
+        const string Name = "m_networking";
+        const BindingFlags Flags = BindingFlags.Static | BindingFlags.NonPublic;
+        static readonly Type Type = typeof(MySteamGameService);
+        static readonly FieldInfo Field = Type.GetField(Name, Flags);
+
+        public static IMyNetworking Value => (IMyNetworking) Field.GetValue(null);
+    }
+}
